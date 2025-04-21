1524cff4316efe92c0fe5afa1496feff9740ecd5
Vedangi-11 <vedupatel1103@gmail.com>
2025-04-21 21:01:56 +0530

    backend link updated

--- transformo-backend/src/app.js
+++ transformo-backend/src/app.js
@@ -9,7 +9,10 @@ const userRoutes=require('./routes/userRoutes');
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'https://transfromo-web-project.vercel.app/', // or '*' during development
+  credentials: true
+}));
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
